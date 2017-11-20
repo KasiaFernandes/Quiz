e6688ac61f6c7f30276170f2a3de1cd789076bec
Bartek Kozera <bartosz.kozera@infor.com>
2017-11-20 20:45:34 +0100

    simplyfing config

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,7 @@ if (env === 'production') {
 
 //webpack.config.js
 module.exports = {
-    entry: (env !== 'production' ? [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-    ] : []).concat(['./client/index.js']),
+    entry: './client/index.js',
     output: {
         filename: './bundle.js',
         path: path.resolve(__dirname, 'public'),
