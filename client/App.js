f58d777250b86bfb39459691ae9ee1ad6cdd2ae8
Bartek Kozera <bartosz.kozera@infor.com>
2017-11-20 20:46:00 +0100

    moving render inside App component

--- client/App.js
+++ client/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import Title from './Title.js';
 
-
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-
-
-
-
+    }
+    // Layout 
+    render() {
+        return (
+            <div>
+                <Title title="Welcome to the quiz!!!!" />
+            </div>
+        )
     }
 }
 
 // render UserForm first, if user names not blank, render layout
-
-// Layout 
-render() {
-    return (
-        <div>
-            <Title title="Welcome to the quiz!" />
-
-        </div>
-    )
-}
 
 export default App;
 
