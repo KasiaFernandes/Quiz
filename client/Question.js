356274db0a49a201d3e36d25a927d46e48be3473
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-11-28 20:48:29 +0100

    add question

--- client/Question.js
+++ client/Question.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Question = props => {
     return (
         <div>
-            <h3>{props.question}</h3>
+            <h3>{props.question.question}</h3>
+            <ul>
+                {props.question.answers.map(answer => <li>{answer.answer}</li>)}
+            </ul>
         </div>
     )
 }
