587160b4766f249cf14217130ad2734fae5fc4d4
Kasia Fernandes <fernandes.kasia@gmail.com>
2018-01-17 20:02:22 +0100

    categories working

--- client/Categories.js
+++ client/Categories.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import style from './Categories.css';
 import Game from './Game.js';
-import QuestionsData from './QuestionsData.js';
 
 class Categories extends React.Component {
     constructor(props) {
@@ -9,28 +8,37 @@ class Categories extends React.Component {
         this.state = {
             category: ''
         }
-        this.renderGame = this.renderGame.bind(this);
+        this.handleCategoryChoice = this.handleCategoryChoice.bind(this);
     }
 
     renderGame() {
         return (
-            <Game name={this.state.name} />
+            <Game name={this.props.name} category={this.state.category} onGameReset={() => this.resetCategories()} />
         )
     }
 
     handleCategoryChoice(category) {
-        this.setState({ category: QuestionsData.category })
+        this.setState({ category: category })
     }
 
-
-
     render() {
+        if ()
+            return (
+                this.state.category === '' ? this.renderCategories() : this.renderGame()
+            )
+    }
+
+    resetCategories() {
+        this.setState({ category: '' })
+    }
+
+    renderCategories() {
         return (
             <div>
                 <h2 className={style.Heading}>Hello, {this.props.name}! Choose a category!</h2>
                 <div className={style.Container}>
-                    <div className={style.Category} onClick={() => this.renderGame()}><h1>JavaScript</h1></div>
-                    <div className={style.Category}><h1>CSS</h1></div>
+                    <div className={style.Category} onClick={() => this.handleCategoryChoice("javascript")}><h1>JavaScript</h1></div>
+                    <div className={style.Category} onClick={() => this.handleCategoryChoice("css")}><h1>CSS</h1></div>
                 </div>
             </div>
         )
@@ -54,6 +62,26 @@ const Categories = props => {
             <h2 className={style.Heading}>Hello, {props.name}! Choose a category!</h2>
             <div className={style.Container}>
                 <div className={style.Category} onClick={() => this.renderGame()}><h1>JavaScript</h1></div>
+                <div className={style.Category}><h1>CSS</h1></div>
+            </div>
+        </div>
+    )
+
+}
+
+const Categories = props => {
+
+    function renderGame(category) {
+        return (
+            <Game name={props.name} category={category} />
+        )
+    }
+
+    return (
+        <div>
+            <h2 className={style.Heading}>Hello, {props.name}! Choose a category!</h2>
+            <div className={style.Container}>
+                <div className={style.Category} onClick={() => renderGame("javascript")}><h1>JavaScript</h1></div>
                 <div className={style.Category}><h1>CSS</h1></div>
             </div>
         </div>
